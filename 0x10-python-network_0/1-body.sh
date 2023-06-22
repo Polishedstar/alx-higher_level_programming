d6bf0c000f3f3ac1d32ed568020e7bb77bfb4cf6
Polishedstar <111003992+Polishedstar@users.noreply.github.com>
2023-06-22 04:40:20 -0700

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+
+# Check if URL is provided as an argument
+if [ -z "$1" ]; then
+    echo "Please provide a URL as an argument."
+    exit 1
+fi
+
+# Store the URL provided as an argument
+url=$1
+
+# Send GET request using curl and store the response body and status code
+response=$(curl -s -w "%{http_code}" -X GET "$url")
+
+# Extract the status code and response body
+status_code=$(echo "$response" | tail -n 1)
+body=$(echo "$response" | head -n -1)
+
+# Display the body of the response only if the status code is 200
+if [ "$status_code" -eq 200 ]; then
+    echo "$body"
+fi
+
